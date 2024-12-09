ce3e980143e0546e1985715efcf10c9e2305add8
Davide Brognoli <davide.brognoli@codiceplastico.com>
2024-12-09 22:27:27 +0100

    Enable test for Part 1 in day9.test.js

--- test/day9.test.js
+++ test/day9.test.js
@@ -3,9 +3,9 @@ const { solvePart1, solvePart2 } = require("../src/day9/index");
 const input = `2333133121414131402`
 
 // Test for Part 1
-/*test("Example case for Part 1", () => {
+test("Example case for Part 1", () => {
   expect(solvePart1(input)).toBe(1928);
-});*/
+});
 
 // Test for Part 2
 test("Example case for Part 2", () => {
